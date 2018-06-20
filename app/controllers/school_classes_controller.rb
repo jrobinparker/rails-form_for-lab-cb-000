ad0794f3939ad638b3ab47b03afe177df9b4eb5c
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 14:42:00 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,4 +1,7 @@
 class SchoolClassesController < ApplicationController
+  before_filter :set_school_class, only: [:show, :edit]
+  before_filter :school_class_params, only: [:create, :update]
+  
   def show
   end
 
@@ -6,11 +9,29 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    @student = Student.new
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def edit
   end
 
   def update
+    @student = Student.update
+    @student.save 
+    redirect_to student_path(@student)
   end
+  
+  private 
+  
+  def set_student 
+    @school_class = SchoolClass.find(params[:id])
+  end 
+  
+  def student_params 
+    params.require(:student).permit(:first_name, :last_name)
+  end 
+  
 end
+
